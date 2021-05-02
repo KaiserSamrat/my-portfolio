45e7205ea5c4a8e1114b50101098595ba945a881
Kaiser Samrat <kaisersamrat136@gmail.com>
2021-05-02 20:10:44 +0600

    Navbar edited

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,27 +1,28 @@
 import React from 'react';
+import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div class='container'>
       <nav class="navbar navbar-expand-lg navbar-dark ">
         <div class="container-fluid">
-          <a class="navbar-brand" href="/">Kaiser</a>
+          <a class="navbar-brand my-name" href="/">Kaiser</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <a class="nav-link active list" aria-current="page" href="/">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link ms-3" href="#footer-area">Contact</a>
+                <a class="nav-link ms-3 list" href="#">Projects</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link ms-3" href="#review-area">Reviews</a>
+                <a class="nav-link ms-3 list" href="#">Blog</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link ms-3" href="#services-area">Courses</a>
+                <a class="nav-link ms-3 list" href="#">Contact</a>
               </li>
 
 
